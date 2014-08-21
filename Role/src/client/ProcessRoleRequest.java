82bceb2c4b2a3edb5eba1876b560906843c985bb
Vinicius Fagundes <vinf100@gmail.com>
2014-08-21 18:11:03 +1000

    Started to create program to process menu

---
+++ Role/src/client/ProcessRoleRequest.java
@@ -0,0 +1,21 @@
+package client;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class ProcessRoleRequest extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
