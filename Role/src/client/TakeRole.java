752c88ad96c5203043bd114cfa99c7cc4229db45
Vinicius Fagundes <vinf100@gmail.com>
2014-08-25 17:27:04 +1000

    Added content to the application to take role

--- Role/src/client/TakeRole.java
+++ Role/src/client/TakeRole.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
@@ -13,20 +14,54 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class TakeRole extends HttpServlet {
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {	
-		String url = null;
-		HttpSession session = request.getSession(false);
+	public void writeValues(String url, HttpSession session, ResultSet rs,
+			PrintWriter out, PreparedStatement prepStmt) {
+		Connection conn = null;
 		try {
-			PrintWriter out = response.getWriter();
-			Connection conn = null;
-			conn =  Menu.setUpConnection(conn, url);
+			conn = Menu.setUpConnection(conn, url);
+
 			String query = "SELECT * FROM ?";
-			PreparedStatement prepStmt = conn.prepareStatement(query);
-			prepStmt.setString(1,(String) session.getAttribute("ClassName"));
+			prepStmt = conn.prepareStatement(query);
+			prepStmt.setString(1, (String) session.getAttribute("ClassName"));
+			rs = prepStmt.executeQuery();
+			while (rs.next()) {
+				out.println("<table><tr><td>" + rs.getString("firstname")
+						+ "</td><td>" + rs.getString("lastname") + "</td></tr></table>");
+			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}finally{
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}finally{
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}finally{
+					try {
+						prepStmt.close();
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
 		}
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		ResultSet rs = null;
+		String url = null;
+		PreparedStatement prepStmt = null;
+		HttpSession session = request.getSession(false);
+		PrintWriter out = response.getWriter();
+		out.println("<html> <head> <title> Take Role </title> </head> <body>"
+				+ "<form action = '/Role/ProcessRoleRequest' method = 'POST'>");
+		writeValues(url, session, rs, out, prepStmt);
 	}
 }
